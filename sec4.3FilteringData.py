8becb042d8fb22ba02f75d73927dfd9edebce9d9
Brad <brad.richardson@transware.net>
2014-08-08 12:54:49 -0500

    Implement example in 4.3

---
+++ sec4.3FilteringData.py
@@ -0,0 +1,30 @@
+import vtk
+from vtk.util.colors import tomato
+
+part = vtk.vtkSTLReader()
+part.SetFileName("../VTKdata/Data/42400-IDGH.stl")
+
+shrink = vtk.vtkShrinkPolyData()
+shrink.SetInputConnection(part.GetOutputPort())
+shrink.SetShrinkFactor(0.85)
+
+partMapper = vtk.vtkPolyDataMapper()
+partMapper.SetInputConnection(shrink.GetOutputPort())
+
+partActor = vtk.vtkLODActor()
+partActor.SetMapper(partMapper)
+
+# Create the graphics structure
+ren1 = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren1)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# Add the actors to the renederer, set the background and size
+ren1.AddActor(partActor)
+ren1.SetBackground(0.1, 0.2, 0.4)
+renWin.SetSize(200, 200)
+
+iren.Initialize()
+iren.Start()
